3bf142ee434be2e5917ec7ed4d9477b6ff1bbe8c
Bread-kn72 <ordinarytom72@gmail.com>
2024-03-22 15:15:42 +0900

    추가 버튼 구현

--- 3rdAssignment To-do List/ViewController.swift
+++ 3rdAssignment To-do List/ViewController.swift
@@ -18,7 +18,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         todoTableView.dataSource = self
         // identifier 수정
         todoTableView.register(nib, forCellReuseIdentifier: cellReuseIdentifier)
-//        registerXib()
     }
     
 //    private func registerXib() {
@@ -40,6 +39,38 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         return cell
     }
     
+    
+    
+    
+    // 추가버튼 구현
+    @IBAction func addTodo(_ sender: Any) {
+        let title = "To-do 추가"
+        let message = "나의 To-do를 적어보세요"
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let cancel = UIAlertAction(title: "취소", style: .cancel)
+        let ok = UIAlertAction(title: "추가", style: .default) { (_) in
+            //확인했을때 처리 할 내용
+            let txt = alert.textFields?.first
+            if txt?.text?.isEmpty != true {
+                Todo.data.append(Todo(id: <#Int#>, title: (txt!.text!), isCompleted: false))
+                print("입력값 \(txt!.text!)")
+            } else {
+                print("입력된 값이 없습니다.")
+            }
+                
+        }
+        
+        alert.addAction(cancel)
+        alert.addAction(ok)
+        
+        self.present(alert, animated: true)
+        
+        alert.addTextField() { (tf) in
+            tf.placeholder = "작성해주세요"
+            tf.isSecureTextEntry = false
+        }
+    }
 }
 
 
