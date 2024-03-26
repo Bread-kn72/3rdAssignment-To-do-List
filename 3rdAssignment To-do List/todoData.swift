3ec29aeb802c2f6488e60d48485d31f5dbd13156
Bread-kn72 <ordinarytom72@gmail.com>
2024-03-26 15:50:34 +0900

    데이터 반복 저장 해결

--- 3rdAssignment To-do List/todoData.swift
+++ 3rdAssignment To-do List/todoData.swift
@@ -11,8 +11,8 @@ extension Todo {
     
     // Todo extension의 data 변수에 데이터를 추가하는 함수
     static func addTodoList(title: String, isCompleted: Bool = false) {
-            let newId = data.count
-            let newTodo = Todo(id: newId, title: title, isCompleted: isCompleted)
-            data.append(newTodo)
-        }
+        let newId = data.count
+        let newTodo = Todo(id: newId, title: title, isCompleted: isCompleted)
+        data.append(newTodo)
+    }
 }
